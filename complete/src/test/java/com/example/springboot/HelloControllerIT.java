e8cad407c6537b370969367dbef170e5f662f533
Ferenc Schmidt <Ferenc_Schmidt@epam.com>
2020-11-11 17:22:54 +0100

    trigger scm with visible change

--- complete/src/test/java/com/example/springboot/HelloControllerIT.java
+++ complete/src/test/java/com/example/springboot/HelloControllerIT.java
@@ -33,6 +33,6 @@ public class HelloControllerIT {
     public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity(base.toString(),
                 String.class);
-        assertThat(response.getBody()).isEqualTo("Meow!");
+        assertThat(response.getBody()).isEqualTo("Meow!Meow!");
     }
 }
